5f922e1e530dd4a3a77867001adb6b9f360f7dea
ankit <ankitrajeshbansal@gmail.com>
2018-04-11 19:55:16 +0530

    Routing

--- Angular2MVC/Angular2MVC/src/app/app.component.js
+++ Angular2MVC/Angular2MVC/src/app/app.component.js
@@ -5,16 +5,27 @@ var __decorate = (this && this.__decorate) || function (decorators, target, key,
     else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
     return c > 3 && r && Object.defineProperty(target, key, r), r;
 };
+var __metadata = (this && this.__metadata) || function (k, v) {
+    if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
+};
 Object.defineProperty(exports, "__esModule", { value: true });
 var core_1 = require("@angular/core");
+var router_1 = require("@angular/router");
 var AppComponent = /** @class */ (function () {
-    function AppComponent() {
+    function AppComponent(router) {
+        this.router = router;
     }
+    //for manually
+    AppComponent.prototype.onLogout = function () {
+        this.router.navigate(['/login']);
+    };
     AppComponent = __decorate([
         core_1.Component({
             selector: "user-app",
-            template: "\n                <div>\n                    <nav class='navbar navbar-inverse'>\n                        <div class='container-fluid'>\n                            <ul class='nav navbar-nav'>\n                                <li><a [routerLink]=\"['home']\">Home</a></li>\n                                <li><a [routerLink]=\"['user']\">Users Management</a></li>\n                            </ul>\n                        </div>\n                    </nav>\n                    <div class='container'>\n                        <router-outlet></router-outlet>\n                    </div>\n                 </div>\n                "
-        })
+            template: "\n                <div>\n                    <nav class='navbar navbar-inverse'>\n                        <div class='container-fluid'>\n                            <ul class='nav navbar-nav'>\n                                <li><a [routerLink]=\"['']\" routerLinkActive=\"active\" [routerLinkActiveOptions]=\"{exact:true}\">Home</a></li>\n                                <li><a [routerLink]=\"['/user']\" routerLinkActive=\"active\">Users Management</a></li>\n                            </ul>\n                        </div>\n                    </nav>\n                    <div class='container'>\n                        <router-outlet></router-outlet>\n                    </div>\n                 </div>\n                <button [routerLink]=\"['/login']\" routerLinkActive=\"hide\"> Logout</button>\n                ",
+            styles: ['.active{ color:white !important}', '.hide{display:none}']
+        }),
+        __metadata("design:paramtypes", [router_1.Router])
     ], AppComponent);
     return AppComponent;
 }());
--- Angular2MVC/Angular2MVC/src/app/app.component.ts
+++ Angular2MVC/Angular2MVC/src/app/app.component.ts
@@ -1,4 +1,5 @@
-﻿import { Component } from "@angular/core"
+﻿import { Component } from "@angular/core";
+import { Router } from "@angular/router";
 
 @Component({
     selector: "user-app",
@@ -7,8 +8,8 @@
                     <nav class='navbar navbar-inverse'>
                         <div class='container-fluid'>
                             <ul class='nav navbar-nav'>
-                                <li><a [routerLink]="['home']">Home</a></li>
-                                <li><a [routerLink]="['user']">Users Management</a></li>
+                                <li><a [routerLink]="['']" routerLinkActive="active" [routerLinkActiveOptions]="{exact:true}">Home</a></li>
+                                <li><a [routerLink]="['/user']" routerLinkActive="active">Users Management</a></li>
                             </ul>
                         </div>
                     </nav>
@@ -16,9 +17,17 @@
                         <router-outlet></router-outlet>
                     </div>
                  </div>
-                `
+                <button [routerLink]="['/login']" routerLinkActive="hide"> Logout</button>
+                `,
+    styles: ['.active{ color:white !important}','.hide{display:none}']
 })
 
 export class AppComponent {
 
+    constructor(private router: Router) { }
+
+    //for manually
+    onLogout() {
+        this.router.navigate(['/login']);
+    }
 }
